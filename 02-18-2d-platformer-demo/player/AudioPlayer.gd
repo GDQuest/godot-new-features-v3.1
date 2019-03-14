7f99ca9ef36a3e64d839d3b7fca3ed544c7b46cb
Guilherme Oliveira <htoguilherme@gmail.com>
2019-03-14 10:26:04 -0300

    Added sound, removed shooting mechanic, polished camera and fixed player
    Unnecessary files were also removed.

--- 02-18-2d-platformer-demo/player/AudioPlayer.gd
+++
@@ -1,6 +0,0 @@
-extends AudioStreamPlayer2D
-
-
-func play_sound(_stream: AudioStreamSample) -> void:
-	stream = _stream
-	play()
--- 02-18-2d-platformer-demo/player/Player.gd
+++ 02-18-2d-platformer-demo/player/Player.gd
@@ -5,56 +5,41 @@ onready var sprite : Sprite = $Sprite
 onready var animation_player : AnimationPlayer = $AnimationPlayer
 onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer2D
 
-export var snap : = false
-export var move_speed : = 250
-export var jump_force : = 500
-export var gravity_force : = 900
-export var slope_slide_threshold : = 50.0
-export var fire_rate : = 4
-export var bullet : PackedScene
-export var sound_jump : AudioStreamSample
-export var sound_shoot : AudioStreamSample
+export var snap := false
+export var move_speed := 250
+export var jump_force := 500
+export var gravity := 900
+export var slope_slide_threshold := 50.0
 
-var velocity : = Vector2()
-
-
-func _ready() -> void:
-	timer.wait_time = 1.0 / fire_rate
+var velocity := Vector2()
 
 
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity_force * delta
-	
-	if snap:
-		velocity = move_and_slide_with_snap(velocity, Vector2(0, 32), Vector2.UP, slope_slide_threshold)
-	else:
-		velocity = move_and_slide(velocity, Vector2.UP, slope_slide_threshold)
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	var direction_x := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	velocity.x = direction_x * move_speed
+
+	if Input.is_action_just_pressed("jump") and snap:
 		velocity.y = -jump_force
-		audio_player.play_sound(sound_jump)
-	if Input.is_action_pressed("shoot") and timer.is_stopped():
-		shoot()
+		snap = false
+		audio_player.play()
 	
-	var motion : = Vector2(0, 0)
-	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	motion.x *= move_speed
-	velocity.x = lerp(velocity.x, motion.x, 0.1)
+	velocity.y += gravity * delta
+	
+	var snap_vector = Vector2(0, 32) if snap else Vector2()
+	velocity = move_and_slide_with_snap(velocity, snap_vector, Vector2.UP, slope_slide_threshold)
+	
+	if is_on_floor() and (Input.is_action_just_released("move_right") or Input.is_action_just_released("move_left")):
+		velocity.y = 0
+
+	var just_landed := is_on_floor() and not snap
+	if just_landed:
+		snap = true
 	
 	update_animation(velocity)
 
 
-func shoot() -> void:
-	timer.start()
-	var new_bullet : = bullet.instance()
-	new_bullet.initialize(Vector2.RIGHT * (-1 if sprite.flip_h else 1))
-	new_bullet.global_position = global_position
-	add_child(new_bullet)
-	audio_player.play_sound(sound_shoot)
-
-
 func update_animation(velocity: Vector2) -> void:
-	var animation : = "idle"
+	var animation := "idle"
 	if abs(velocity.x) > 10.0:
 		sprite.flip_h = velocity.x < 0
 		animation = "run"
